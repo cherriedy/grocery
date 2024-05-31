aea54a77928dfa1d6848b2e2d381b10322ac080e
cherriedy <huy5adv@gmail.com>
2024-06-01 00:24:16 +0700

    UPDATE: findViewById -> ViewBinding

--- app/src/main/java/com/example/doanmonhoc/activity/ProductManagement/ProductManagementActivity.java
+++ app/src/main/java/com/example/doanmonhoc/activity/ProductManagement/ProductManagementActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,23 +13,17 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.doanmonhoc.adapter.ProductListAdapter;
-import com.example.doanmonhoc.model.Product;
 import com.example.doanmonhoc.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.example.doanmonhoc.adapter.ProductListAdapter;
+import com.example.doanmonhoc.databinding.ActivityProductManagementBinding;
+import com.example.doanmonhoc.model.Product;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProductManagementActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-
-    private FloatingActionButton fabAdd;
-    private FloatingActionButton fabAddOne;
-    private FloatingActionButton fabAddMany;
-    private TextView txtAddOne;
-    private TextView txtAddMany;
+    ActivityProductManagementBinding binding;
 
     // This check if all sub fabs are visible or not
     private boolean isSubMenuOpen;
@@ -38,31 +31,41 @@ public class ProductManagementActivity extends AppCompatActivity {
     private Animation animFromBottomFab;
     private Animation animToBottomFab;
     private Animation animRotateClockWise;
-    private Animation animRotateAntiCloseWise;
+    private Animation animRotateAntiClockWise;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_product_management);
+
+        // Thiết lập ViewBinding
+        binding = ActivityProductManagementBinding.inflate(getLayoutInflater());
+        // Gán layout cho activity
+        setContentView(binding.getRoot());
+
+//        setContentView(R.layout.activity_product_management);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        recyclerView = findViewById(R.id.product_list);
+        // Tạo Adpater cho productList (RecyclerView)
         ProductListAdapter productListAdapter = new ProductListAdapter(ProductManagementActivity.this);
+        // Tạo LinearLayoutManager để quản lý các item
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(ProductManagementActivity.this, RecyclerView.VERTICAL, false);
-        recyclerView.setLayoutManager(linearLayoutManager);
+        // Thiết lập LayoutManager
+        binding.productList.setLayoutManager(linearLayoutManager);
 
+        // Gán mảng dữ liệu cho Adapter thông qua gọi phương thức getListData
         productListAdapter.setData(getListData());
-        recyclerView.setAdapter(productListAdapter);
+        // Gán Adapter cho productView
+        binding.productList.setAdapter(productListAdapter);
 
-        initializeView();
         initializeAnimation();
 
-        fabAdd.setOnClickListener(v -> {
+        binding.addButton.setOnClickListener(v -> {
             if (!isSubMenuOpen) {
                 openMenu();
             } else {
@@ -70,42 +73,34 @@ public class ProductManagementActivity extends AppCompatActivity {
             }
         });
 
-        fabAddOne.setOnClickListener(v -> {
+        binding.addOne.setOnClickListener(v -> {
             Intent intent = new Intent(ProductManagementActivity.this, AddProductActivity.class);
             startActivity(intent);
         });
-    }
-
-    private void initializeView() {
-        fabAdd = findViewById(R.id.add_button);
-        fabAddOne = findViewById(R.id.add_one);
-        fabAddMany = findViewById(R.id.add_many);
-        txtAddOne = findViewById(R.id.add_one_txt);
-        txtAddMany = findViewById(R.id.add_many_txt);
     }
 
     private void initializeAnimation() {
         animFromBottomFab = AnimationUtils.loadAnimation(ProductManagementActivity.this, R.anim.from_bottom_fab);
         animToBottomFab = AnimationUtils.loadAnimation(ProductManagementActivity.this, R.anim.to_bottom_fab);
         animRotateClockWise = AnimationUtils.loadAnimation(ProductManagementActivity.this, R.anim.rotate_clock_wise);
-        animRotateAntiCloseWise = AnimationUtils.loadAnimation(ProductManagementActivity.this, R.anim.rotate_anti_clock_wise);
+        animRotateAntiClockWise = AnimationUtils.loadAnimation(ProductManagementActivity.this, R.anim.rotate_anti_clock_wise);
     }
 
     private void closeMenu() {
-        fabAdd.startAnimation(animRotateAntiCloseWise);
-        fabAddOne.startAnimation(animToBottomFab);
-        fabAddMany.startAnimation(animToBottomFab);
-        txtAddOne.startAnimation(animToBottomFab);
-        txtAddMany.startAnimation(animToBottomFab);
+        binding.addButton.startAnimation(animRotateAntiClockWise);
+        binding.addOne.startAnimation(animToBottomFab);
+        binding.addMany.startAnimation(animToBottomFab);
+        binding.addOneTxt.startAnimation(animToBottomFab);
+        binding.addManyTxt.startAnimation(animToBottomFab);
         isSubMenuOpen = false;
     }
 
     private void openMenu() {
-        fabAdd.startAnimation(animRotateClockWise);
-        fabAddOne.startAnimation(animFromBottomFab);
-        fabAddMany.startAnimation(animFromBottomFab);
-        txtAddOne.startAnimation(animFromBottomFab);
-        txtAddMany.startAnimation(animFromBottomFab);
+        binding.addButton.startAnimation(animRotateClockWise);
+        binding.addOne.startAnimation(animFromBottomFab);
+        binding.addMany.startAnimation(animFromBottomFab);
+        binding.addOneTxt.startAnimation(animFromBottomFab);
+        binding.addManyTxt.startAnimation(animFromBottomFab);
         isSubMenuOpen = true;
     }
 
