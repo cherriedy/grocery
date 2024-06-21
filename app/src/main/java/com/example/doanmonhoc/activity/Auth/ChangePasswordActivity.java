6528dd163d56023074156891fca6859db7ec80ed
AnhKiet1234 <2254810125@vaa.edu.vn>
2024-06-21 19:24:10 +0700

    them layout quen mat khau

---
+++ app/src/main/java/com/example/doanmonhoc/activity/Auth/ChangePasswordActivity.java
@@ -0,0 +1,15 @@
+package com.example.doanmonhoc.activity.Auth;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.doanmonhoc.R;
+
+public class ChangePasswordActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_changepassword);
+        }
+}
--- app/src/main/java/com/example/doanmonhoc/activity/Auth/LoginActivity.java
+++ app/src/main/java/com/example/doanmonhoc/activity/Auth/LoginActivity.java
@@ -3,10 +3,13 @@ package com.example.doanmonhoc.activity.Auth;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.doanmonhoc.R;
@@ -30,6 +33,7 @@ public class LoginActivity extends AppCompatActivity {
         EditText edtUsername = findViewById(R.id.edtUsername);
         EditText edtPassword = findViewById(R.id.edtPassword);
         Button btnLogin = findViewById(R.id.btnLogin);
+        TextView quenMatKhau = findViewById(R.id.quenMatKhau);
 
         btnLogin.setOnClickListener(v -> {
             String username = edtUsername.getText().toString().trim();
@@ -39,6 +43,14 @@ public class LoginActivity extends AppCompatActivity {
                 login(username, password);
             } else {
                 Toast.makeText(LoginActivity.this, "Hãy nhập username và mật khẩu", Toast.LENGTH_SHORT).show();
+            }
+        });
+        quenMatKhau.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
+                // Start the new activity
+                startActivity(intent);
             }
         });
     }
